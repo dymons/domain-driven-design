84a3f82f29dc3d2d28f4375a7ad4300852536618
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-05-13 22:02:58 +0300

    Fix

--- service_template/third_party/userver/samples/grpc_middleware_service/src/middlewares/auth.cpp
+++
@@ -1,8 +0,0 @@
-#include "auth.hpp"
-
-namespace sample::grpc::auth {
-
-const ::grpc::string kKey = "x-key";
-const ::grpc::string kCredentials = "secret-credentials";
-
-}  // namespace sample::grpc::auth
--- service_template/third_party/userver/scripts/docs/doxygen-awesome-css/README.md
+++
@@ -1,5 +0,0 @@
-# Doxygen Awesome
-
-Doxygen Awesome repository: [link](https://github.com/jothepro/doxygen-awesome-css)
-
-Version 2.2.1 without changes
