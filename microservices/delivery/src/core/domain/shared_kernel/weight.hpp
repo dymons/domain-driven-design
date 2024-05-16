08f71ebe2698cb134b4dd5414565dd56144867f6
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-05-16 23:34:21 +0300

    Add comments to the Weight VO

--- microservices/delivery/src/core/domain/shared_kernel/weight.hpp
+++ microservices/delivery/src/core/domain/shared_kernel/weight.hpp
@@ -5,18 +5,56 @@
 namespace delivery::core::domain::shared_kernel {
 
 class Weight {
+  /// Weight, stores the value in kilograms
   int weight_;
 
+  // Constructors
+
+  /*!
+    \brief
+        User constructor, constructs an Weight with predefined weight.
+  */
   explicit Weight(int weight) : weight_(weight) {}
 
  public:
+  /*!
+    \brief
+        Named constructor, constructs a Weight with predefined weight
+
+    \param weight
+        The weight in kilograms
+
+    \throw InvalidArgument
+        During construction, check that the weight takes a positive value
+
+    \return
+        Return a validate weight value object
+
+    \ingroup
+        SharedKernel
+
+    \code
+        auto const weight = Weight::Create(10);
+        do_some_stuff(weight.GetWeight());
+    \endcode
+  */
   [[nodiscard]] static auto Create(int weight) -> Weight;
 
+  // Observers
+
+  /*!
+    \brief
+        Get the weight value of the value object
+  */
   template <typename Self>
   [[nodiscard]] constexpr decltype(auto) GetWeight(this Self&& self) {
     return std::forward<Self>(self).weight_;
   }
 
+  /*!
+    \brief
+        Operator for comparing two Weight objects
+  */
   constexpr auto operator<=>(Weight const&) const = default;
 };
 
