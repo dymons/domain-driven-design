4876bfc8fabb373103232698b64f0994526a50e2
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-05-16 22:17:45 +0300

    Add a tests for Weight

--- microservices/delivery/src/core/domain/shared_kernel/weight_test.cpp
+++ microservices/delivery/src/core/domain/shared_kernel/weight_test.cpp
@@ -14,4 +14,52 @@ UTEST(WeightShould, BeCorrectWhenParamsIsCorrectOnCreated) {
   EXPECT_EQ(weight.GetWeight(), 10);
 }
 
+UTEST(WeightShould, BeEqualWhenAllPropertiesIsEqual) {
+  // Arrange
+  auto const first = Weight::Create(10);
+  auto const second = Weight::Create(10);
+
+  // Act
+  auto const result = first == second;
+
+  // Assert
+  EXPECT_TRUE(result);
+}
+
+UTEST(WeightShould, BeNotEqualWhenAllPropertiesIsEqual) {
+  // Arrange
+  auto const first = Weight::Create(10);
+  auto const second = Weight::Create(5);
+
+  // Act
+  auto const result = first == second;
+
+  // Assert
+  EXPECT_FALSE(result);
+}
+
+UTEST(WeightShould, CanCompareMoreThen) {
+  // Arrange
+  auto const first = Weight::Create(10);
+  auto const second = Weight::Create(5);
+
+  // Act
+  auto const result = first > second;
+
+  // Assert
+  EXPECT_TRUE(result);
+}
+
+UTEST(WeightShould, CanCompareLessThen) {
+  // Arrange
+  auto const first = Weight::Create(10);
+  auto const second = Weight::Create(5);
+
+  // Act
+  auto const result = first < second;
+
+  // Assert
+  EXPECT_FALSE(result);
+}
+
 }  // namespace delivery::core::domain::shared_kernel
