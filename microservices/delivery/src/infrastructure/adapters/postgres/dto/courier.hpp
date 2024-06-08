7126ec0cf2f6d92087d0c6523cf18c10fea1923d
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-06-08 14:38:36 +0300

    Fix

--- microservices/delivery/src/infrastructure/adapters/postgres/dto/courier.hpp
+++ microservices/delivery/src/infrastructure/adapters/postgres/dto/courier.hpp
@@ -8,11 +8,11 @@
 namespace delivery::infrastructure::adapters::postgres::dto {
 
 struct Courier final {
-  std::string id;
-  std::string name;
-  std::string status;
-  Transport transport;
-  Location current_location;
+  std::string id{};
+  std::string name{};
+  std::string status{};
+  Transport transport{};
+  Location current_location{};
 };
 
 }  // namespace delivery::infrastructure::adapters::postgres::dto
