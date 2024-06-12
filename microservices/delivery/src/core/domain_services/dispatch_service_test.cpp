6c96fd1941ce978b95dc4eacf3340cf7ee1c9924
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-06-12 09:50:56 +0300

    Fix

--- microservices/delivery/src/core/domain_services/dispatch_service_test.cpp
+++ microservices/delivery/src/core/domain_services/dispatch_service_test.cpp
@@ -1,7 +1,13 @@
 #include <userver/utest/utest.hpp>
 
+#include "dispatch_service.hpp"
+
 namespace delivery::core::domain_services {
 
+namespace {
 
+UTEST(DispatchServiceShould, NotDispatchWhenNotReadyCouriers) {}
 
-} // namespace delivery::core::domain_services
+}  // namespace
+
+}  // namespace delivery::core::domain_services
