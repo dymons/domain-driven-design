29f551f8ea0ca155206fc4b51427ebff66f76d55
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-06-12 10:26:24 +0300

    Fix

--- microservices/delivery/src/core/domain/order/order_mock_test.cpp
+++ microservices/delivery/src/core/domain/order/order_mock_test.cpp
@@ -5,8 +5,8 @@
 namespace delivery::core::domain::order {
 
 Order MockOrder() {
-  static const auto kBasketId = userver::utils::generators::GenerateUuidV7();
-  return Order::Create(BasketId{kBasketId}, Location{X{5}, Y{5}}, Weight{10});
+  return Order::Create(BasketId{userver::utils::generators::GenerateUuidV7()},
+                       Location{X{5}, Y{5}}, Weight{10});
 }
 
 }  // namespace delivery::core::domain::order
