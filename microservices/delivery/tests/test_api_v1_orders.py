9d700b0d106777eb75a38d5caec82d9160490ab8
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-06-16 21:32:15 +0300

    Fix

--- microservices/delivery/tests/test_api_v1_orders.py
+++ microservices/delivery/tests/test_api_v1_orders.py
@@ -1,8 +1,6 @@
 async def test_basic(service_client):
-    response = await service_client.post('/api/v1/orders',
-                                         json={
-                                             'basket_id': 'basket_id1',
-                                             'address': 'address',
-                                             'weight': 1
-                                         })
+    response = await service_client.post(
+        '/api/v1/orders',
+        json={'basket_id': 'basket_id1', 'address': 'address', 'weight': 1}
+    )
     assert response.status == 200
