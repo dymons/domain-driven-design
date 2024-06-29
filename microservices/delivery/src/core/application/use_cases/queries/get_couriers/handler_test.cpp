c75a697d9c60b7bc7bee92d8d582be2293bb8fae
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-06-29 23:40:19 +0300

    Fix

---
+++ microservices/delivery/src/core/application/use_cases/queries/get_couriers/handler_test.cpp
@@ -0,0 +1,11 @@
+#include <userver/utest/utest.hpp>
+
+namespace delivery::core::application::use_cases::queries::get_couriers {
+
+namespace {
+
+UTEST(GetCouriersHandlerShould, Empty) {}
+
+}  // namespace
+
+}  // namespace delivery::core::application::use_cases::queries::get_couriers
---
+++ microservices/delivery/src/core/application/use_cases/queries/get_orders/handler_test.cpp
@@ -0,0 +1,11 @@
+#include <userver/utest/utest.hpp>
+
+namespace delivery::core::application::use_cases::queries::get_orders {
+
+namespace {
+
+UTEST(GetOrdersHandlerShould, Empty) {}
+
+}  // namespace
+
+}  // namespace delivery::core::application::use_cases::queries::get_orders
