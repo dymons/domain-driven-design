1fdce288cc313d50f3042b2e4708f43e2effde70
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-06-30 19:45:07 +0300

    Fix

--- microservices/delivery/tests/delivery/test_api_v1_orders.py
+++ microservices/delivery/tests/delivery/test_api_v1_orders.py
@@ -1,6 +1,11 @@
 from microservices.delivery.tests.fixtures import api_v1_orders
 
 
-async def test_basic(api_v1_orders):
+async def test_given_empty_orders_when_create_order_then_order_is_created(
+        api_v1_orders,
+):
+    # Arrange & Act
     response = await api_v1_orders.execute()
+
+    # Assert
     assert response.status == 200
