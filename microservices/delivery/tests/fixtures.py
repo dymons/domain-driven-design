262b2058692e473482be7371fb905c3a3322b531
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-06-30 19:35:53 +0300

    Fix

--- microservices/delivery/tests/fixtures.py
+++ microservices/delivery/tests/fixtures.py
@@ -1,17 +1,19 @@
 import pytest
 
 
+from microservices.delivery.tests.requests import get_api_v1_orders_request
+
+
 @pytest.fixture
 async def api_v1_orders(
         service_client,
 ):
-    request = {
-        'basket_id': 'basket_id1',
-        'address': 'address',
-        'weight': 1,
-    }
+    request = get_api_v1_orders_request()
 
     class Context:
+        def __init__(self):
+            pass
+
         # noinspection PyMethodMayBeStatic
         async def execute(self):
             return await service_client.post('/api/v1/orders', json=request)
---
+++ microservices/delivery/tests/requests.py
@@ -0,0 +1,6 @@
+def get_api_v1_orders_request():
+    return {
+        'basket_id': 'basket_id',
+        'address': 'address',
+        'weight': 1,
+    }
