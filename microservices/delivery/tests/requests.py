1e53901eda867c27d62e3e5b0823ca02b746b322
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-06-30 22:09:39 +0300

    Fix

--- microservices/delivery/tests/requests.py
+++ microservices/delivery/tests/requests.py
@@ -1,3 +1,6 @@
+from typing import Dict
+from typing import Any
+
 
 def default_basket_id() -> str:
     return 'default_basket_id'
@@ -7,7 +10,7 @@ def default_api_v1_orders_request(
         basket_id: str = default_basket_id(),
         address: str = 'address',
         weight: int = 1,
-):
+) -> Dict[str, Any]:
     return {
         'basket_id': basket_id,
         'address': address,
