71b2c08d24d0b0c81a4deae12ddd93e6a6f192d1
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-06-30 21:14:23 +0300

    Fix

--- microservices/delivery/tests/delivery/test_api_v1_orders.py
+++ microservices/delivery/tests/delivery/test_api_v1_orders.py
@@ -15,3 +15,19 @@ async def test_given_empty_orders_when_create_order_then_order_is_created(
     assert order_repository.get_orders() == [
         (fake_basket_id(), 'created', None, '(1,1)', '(1)'),
     ]
+
+
+async def test_given_empty_orders_when_several_times_create_order_then_order_is_created_once(
+        api_v1_orders,
+        order_repository,
+):
+    # Arrange & Act
+    response_first = await api_v1_orders.execute()
+    response_second = await api_v1_orders.execute()
+
+    # Assert
+    assert response_first.status == 200
+    assert response_second.status == 200
+    assert order_repository.get_orders() == [
+        (fake_basket_id(), 'created', None, '(1,1)', '(1)'),
+    ]
