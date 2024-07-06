882cb3e3772219ffcf2477008e756ef4ab6e4c12
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-07-06 19:54:30 +0300

    Fix

--- microservices/delivery/tests/delivery/utils/types.py
+++ microservices/delivery/tests/delivery/utils/types.py
@@ -1,6 +1,12 @@
-def default_basket_id() -> str:
-    return 'default_basket_id'
+from typing import NewType
+
+BasketId = NewType('basket_id', str)
+CourierId = NewType('courier_id', str)
 
 
-def default_courier_id() -> str:
-    return 'default_courier_id'
+def default_basket_id() -> BasketId:
+    return BasketId('default_basket_id')
+
+
+def default_courier_id() -> CourierId:
+    return CourierId('default_courier_id')
