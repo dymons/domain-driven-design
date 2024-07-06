8467f8cbc0dbe2ef12879d1f7e26a0d31e792cea
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-07-06 18:01:45 +0300

    Fix

--- microservices/delivery/tests/delivery/test_api_v1_couriers.py
+++ microservices/delivery/tests/delivery/test_api_v1_couriers.py
@@ -4,13 +4,10 @@ from microservices.delivery.tests.delivery.fixtures.courier_repository import co
 
 async def test_empty_couriers(
         api_v1_couriers,
-        courier_repository,
 ):
     # Arrange & Act
     response = await api_v1_couriers.execute()
 
     # Assert
     assert response.status == 200
-    # assert order_repository.get_orders() == [
-    #     (default_basket_id(), 'created', None, '(1,1)', '(1)'),
-    # ]
+    assert response.json() == {'couriers': []}
