5c24c112a00a565ea03fc30db0116dbd7d630d5f
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-07-06 16:48:18 +0300

    Fix

--- microservices/delivery/tests/delivery/fixtures/courier_repository.py
+++ microservices/delivery/tests/delivery/fixtures/courier_repository.py
@@ -1,15 +1,28 @@
 import pytest
 
+from typing import List
+
 
 @pytest.fixture
 async def courier_repository(pgsql):
     class Context:
         def __init__(self):
-            pass
+            self.db = '20240526213109_init'
 
-        @staticmethod
-        def get_couriers():
-            cursor = pgsql['20240526213109_init'].cursor()
+        def insert_couriers(self, couriers: List):
+            rows = ",".join(map(str, couriers))
+            cursor = pgsql[self.db].cursor()
+            try:
+                cursor.execute(f"""
+                    INSERT INTO delivery.couriers (id, name, status, transport, current_location) 
+                    VALUES {rows}
+                """)
+                return cursor.fetchall()
+            finally:
+                cursor.close()
+
+        def get_couriers(self):
+            cursor = pgsql[self.db].cursor()
             try:
                 cursor.execute('SELECT * FROM delivery.couriers')
                 return cursor.fetchall()
