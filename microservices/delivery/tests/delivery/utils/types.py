be875ceeed73293157c80f0bdba59185fddd4e07
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-07-07 12:58:31 +0300

    Fix

--- microservices/delivery/tests/delivery/utils/types.py
+++ microservices/delivery/tests/delivery/utils/types.py
@@ -41,3 +41,21 @@ def make_courier(
         'transport': transport,
         'current_location': current_location,
     }
+
+
+def make_order(
+        id: str = Defaults.basket_id,
+        status: str = Defaults.order_status,
+        courier_id: Optional[str] = None,
+        delivery_location: Optional[Dict[str, Any]] = None,
+        weight: int = Defaults.weight,
+):
+    if delivery_location is None:
+        delivery_location = Defaults.delivery_location
+    return {
+        'id': id,
+        'status': status,
+        'courier_id': courier_id,
+        'delivery_location': delivery_location,
+        'weight': weight,
+    }
