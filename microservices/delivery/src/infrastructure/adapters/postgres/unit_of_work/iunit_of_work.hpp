b52a52fd73686de5f7068cba14cdcc4f7a0afde9
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2024-09-01 18:12:20 +0300

    Fix

--- microservices/delivery/src/infrastructure/adapters/postgres/unit_of_work/iunit_of_work.hpp
+++ microservices/delivery/src/infrastructure/adapters/postgres/unit_of_work/iunit_of_work.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <functional>
+
+#include <core/ports/courier_repository/fwd.hpp>
+#include <core/ports/order_repository/fwd.hpp>
+#include <utils/memory.hpp>
 #include <utils/non_copyable_and_moveable.hpp>
 
 #include "fwd.hpp"
@@ -9,6 +14,14 @@ namespace delivery::infrastructure::adapters::postgres {
 class IUnitOfWork : private NonCopyableAndMoveable {
  public:
   virtual ~IUnitOfWork() = default;
+
+  virtual auto RunTransaction(std::function<void()>) -> void = 0;
+
+  [[nodiscard]] virtual auto GetCourierRepository()
+      -> SharedRef<core::ports::ICourierRepository> = 0;
+
+  [[nodiscard]] virtual auto GetOrderRepository()
+      -> SharedRef<core::ports::IOrderRepository> = 0;
 };
 
 }  // namespace delivery::infrastructure::adapters::postgres
